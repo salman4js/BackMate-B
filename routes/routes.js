1fbb1924bcbbb1389961d1eb458fa709f919c85c
Salman_JS <salman4javascript@gmail.com>
2023-02-04 00:52:56 +0530

    Added some routes for testing

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // Importing required controllers
 const userController = require("../controllers/authController.js");
+const specController = require("../spec/spec.js");
 
 // Autentication Routes!
 router.get("/allusers", userController.allUsers);
 router.post("/createuser", userController.createUser);
 router.post("/loginuser", userController.loginUser);
-router.delete("/:id/deleteuser", userController.deleteUser)
+router.delete("/:id/deleteuser", userController.deleteUser);
+
+// Testing routes -- Delete later!
+router.get("/alluserstest/", specController.specTest);
 
 module.exports = router;
